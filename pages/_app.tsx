3104ce943a2cc2e2b51b22f91747e90dde41697e
Kasper Juul Hermansen <contact@kjuulh.io>
2021-05-24 17:27:51 +0200

    Add tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,16 @@ import UserProvider from "../context/userContext";
 import { Provider } from "react-redux";
 
 import store from "../lib/redux/store";
+import "tailwindcss/tailwind.css";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Provider store={store}>
-      <UserProvider>
-        <Component {...pageProps} />
-      </UserProvider>
-    </Provider>
+    <>
+      <Provider store={store}>
+        <UserProvider>
+          <Component {...pageProps} />
+        </UserProvider>
+      </Provider>
+    </>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,17 @@ const HomePage = () => {
   const router = useRouter();
 
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className="min-h-screen bg-gray-100">
+      <h1 className="text-yellow-400 text-4xl uppercase font-semibold">
+        Rawpotion
+      </h1>
       <div>
-        <button onClick={() => router.push("/login")}>Login</button>
+        <button
+          className="border border-2 border-yellow-400 uppercase font-medium text-yellow-400 px-4 py-1 rounded-md text-lg"
+          onClick={() => router.push("/login")}
+        >
+          Login
+        </button>
         <button onClick={() => router.push("/register")}>Register</button>
       </div>
     </div>
