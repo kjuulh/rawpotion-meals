fbf542d564c5ca0e8f8cef14c42a8aed87c1ddae
kjuulh <contact@kjuulh.io>
2021-06-13 13:53:20 +0200

    Add console.log

--- lib/firebase/clientApp.tsx
+++ lib/firebase/clientApp.tsx
@@ -15,6 +15,8 @@ const clientCredentials = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
+console.log(clientCredentials);
+
 if (!firebase.apps.length) {
   firebase.initializeApp(clientCredentials);
   // Check that `window` is in scope for the analytics module!
