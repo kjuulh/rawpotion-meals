5dfdb9be4b3eda0b59438602c8e2779f54736e0c
kjuulh <contact@kjuulh.io>
2021-08-17 22:53:54 +0200

    Removed sentry

--- backend/RawPotion.Meals/RawPotion.Meals.Application/Features/Groups/Queries/GetGroupsForUser/GroupVm.cs
+++ backend/RawPotion.Meals/RawPotion.Meals.Application/Features/Groups/Queries/GetGroupsForUser/GroupVm.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using RawPotion.Meals.Application.Common.Mappings;
-using RawPotion.Meals.Application.Features.Meals.Commands.CreateMealForGroup;
+using RawPotion.Meals.Application.Features.Meals.Queries.GetMealById;
 using RawPotion.Meals.Application.Features.Users.Commands.RegisterUser;
 using RawPotion.Meals.Domain.Entities;
 
@@ -22,6 +22,25 @@ namespace RawPotion.Meals.Application.Features.Groups.Queries.GetGroupsForUser
         public IEnumerable<UserVm> Members { get; set; }
 
         [Required]
-        public IEnumerable<MealBriefVm> Meals { get; set; }
+        public IEnumerable<MealWithoutGroupVm> Meals { get; set; }
+    }
+    
+    
+    public class MealWithoutGroupVm : IMapFrom<Meal>
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public UserVm Host { get; set; }
+
+        [Required]
+        public string Recipe { get; set; }
+
+        [Required]
+        public string Date { get; set; }
+        
+        [Required]
+        public IEnumerable<UserVm> ParticipatingMembers { get; set; }
     }
 }
