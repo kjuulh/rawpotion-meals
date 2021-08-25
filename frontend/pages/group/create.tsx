b02e1a2de013602fb1cdf3e62f3dfd7c54c781f3
kjuulh <contact@kjuulh.io>
2021-08-25 21:02:52 +0200

    Extracted the page itself

--- frontend/pages/group/create.tsx
+++ frontend/pages/group/create.tsx
@@ -1,67 +1,8 @@
-import { Form } from "react-final-form";
 import React from "react";
 import DashboardLayout from "@components/layouts/dashboardLayout";
-import { DashboardHeading, DashboardSection } from "@features/dashboard";
-import BreadCrumbs from "@components/layouts/breadCrumbs";
-import {
-  Card,
-  CardFormInput,
-  Heading,
-  PrimaryButton,
-} from "@components/common";
-import { CardActionArea } from "@components/common/card/cardActionArea";
-import { VerticalSpacer } from "@components/common/modifiers/verticalSpacer";
-import { useCreateGroup } from "@features/groups/create/useCreateGroup";
+import { CreateGroup } from "@features/groups/create/createGroup";
 
-const CreateGroupPage: any = () => {
-  const [createGroup, { isLoading, isSuccess, isError }] = useCreateGroup();
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (isError) {
-    return <div>Error...</div>;
-  }
-
-  if (isSuccess) {
-    return <div>Redirecting</div>;
-  }
-
-  return (
-    <>
-      <DashboardHeading>
-        <Heading>Create Group</Heading>
-        <BreadCrumbs />
-      </DashboardHeading>
-
-      <DashboardSection>
-        <Card>
-          <Form
-            onSubmit={createGroup}
-            render={({ handleSubmit }) => (
-              <form onSubmit={handleSubmit}>
-                <VerticalSpacer amount={4}>
-                  <CardFormInput
-                    name="groupName"
-                    label="Group name"
-                    required
-                    placeholder="What is the name of your future group?"
-                    type="text"
-                  />
-
-                  <CardActionArea>
-                    <PrimaryButton>Create</PrimaryButton>
-                  </CardActionArea>
-                </VerticalSpacer>
-              </form>
-            )}
-          />
-        </Card>
-      </DashboardSection>
-    </>
-  );
-};
+const CreateGroupPage: any = () => <CreateGroup />;
 
 CreateGroupPage.Layout = DashboardLayout;
 
