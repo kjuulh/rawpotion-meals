07de8c061e6254d81486b27f3d9d91a47db6ee71
kjuulh <contact@kjuulh.io>
2021-08-25 21:34:58 +0200

    Add cypress

---
+++ tests/e2e/cypress/integration/load-homepage.spec.js
@@ -0,0 +1,23 @@
+/*eslint-disable */
+
+describe('Home page', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:3000/');
+    })
+
+    it('should load', function () {
+        cy.get('h1').contains("Rawpotion Mealplanner")
+    });
+
+    it('should be possible to click on login', function () {
+        cy.get("button").contains('Login').click()
+
+        cy.url().should("include","/login")
+    });
+
+    it('should be possible to click on Register', function () {
+        cy.get("button").contains('Register').click()
+
+        cy.url().should("include","/register")
+    });
+})
