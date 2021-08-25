4f97497279bef9958a8dd4283abc24524c809473
kjuulh <contact@kjuulh.io>
2021-08-25 22:50:43 +0200

    Add build and cleanup

--- tests/e2e/cypress/integration/load-homepage.spec.js
+++ tests/e2e/cypress/integration/load-homepage.spec.js
@@ -2,7 +2,7 @@
 
 describe('Home page', () => {
     beforeEach(() => {
-        cy.visit('http://localhost:3000/');
+        cy.visit(Cypress.env('CYPRESS_BASE_URL'));
     })
 
     it('should load', function () {
